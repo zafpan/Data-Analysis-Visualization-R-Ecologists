4dc312d46c19e744561a87d4d8ab377e4e9758c7
zafpano <pzaf@ymail.com>
2024-03-26 20:09:08 +0200

    Start script comparing fish length and scale size

---
+++ scripts/fish-analysis.R
@@ -0,0 +1,7 @@
+# Introduce yourself to Git
+library(usethis)
+use_git_config(user.name = "zafpano", user.email = "pzaf@ymail.com")
+
+# Read in data to new R script
+library(dplyr)
+fish_data <- read.csv("data_raw/Gaeta_etal_CLC_data.csv")
