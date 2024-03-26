5d2a7a089e7eddfbb6b0f9442716d476876b3f50
Panos Z <24545681+zafpan@users.noreply.github.com>
2024-03-26 22:15:16 +0200

    Update fish-analysis.R
    change the size threshold of length_cat to 250

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -8,7 +8,7 @@ fish_data <- read.csv("data_raw/Gaeta_etal_CLC_data_1.csv")
 
 # Create new categorical size column
 fish_data_cat <- fish_data %>% 
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
+  mutate(length_cat = ifelse(length > 250, "big", "small"))
 
 # Keep the fish with a scalelength of more than 1 mm
 fish_data_lgth_1mm_plus <- fish_data %>% 
