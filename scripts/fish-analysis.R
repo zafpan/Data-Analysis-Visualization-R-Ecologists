7250315ab89a243529a68c43a687257c81aaf201
zafpano <pzaf@ymail.com>
2024-03-26 20:14:34 +0200

    Add categorical fish length column

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -5,3 +5,6 @@ use_git_config(user.name = "zafpano", user.email = "pzaf@ymail.com")
 # Read in data to new R script
 library(dplyr)
 fish_data <- read.csv("data_raw/Gaeta_etal_CLC_data.csv")
+
+fish_data_cat <- fish_data %>% 
+  mutate(length_cat = ifelse(length > 200, "big", "small"))
